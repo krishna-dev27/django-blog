d844327434c9682b87d4cf193c293318474f8cad
krishna-dev27 <krishnaegv@gmail.com>
2026-07-10 15:04:28 +0530

    Blog model and app created

---
+++ blogs/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+
+# Register your models here.
+from blogs.models import *
+class BlogAdmin(admin.ModelAdmin):
+    prepopulated_fields = {
+        'slug': ('title',),
+        
+    }
+    list_display = ('author','title', 'is_featured')
+    search_fields = ('id', 'title')
+    list_editable = ('is_featured',)
+
+
+admin.site.register(Category)
+admin.site.register(Blog,BlogAdmin)

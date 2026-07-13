c72dc622aacb13bc74df1b7aa0d97128b851b9a4
krishna-dev27 <krishnaegv@gmail.com>
2026-07-13 11:34:27 +0530

    updated

--- blog_main/views.py
+++ blog_main/views.py
@@ -4,7 +4,10 @@ from blogs.models import *
 
 def home(request):
     categories = Category.objects.all()
+    featured_posts = Blog.objects.filter(is_featured = True)
     d={
-        'categories':categories
+        'categories':categories,
+        'featured_posts':featured_posts,
     }
+    
     return render(request,'home.html',d)

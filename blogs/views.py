ca60323f0c3a33675fc0267e422f5a2d6e4a6a43
krishna-dev27 <krishnaegv@gmail.com>
2026-07-21 11:42:00 +0530

    updated redirection

--- blogs/views.py
+++ blogs/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from blogs.models import Blog,Category
 
@@ -7,8 +7,13 @@ def posts_by_category(request,category_id):
     #PO = Category.objects.filter(id=category_id)
     #posts = Blog.objects.filter(category_id = category_id,status='Published')
     posts = Blog.objects.filter(category = category_id,status='Published')
+    try:
+        cname= Category.objects.get(id = category_id)
+    except:
+        return redirect('home')
     context = {
-        'posts':posts
+        'posts':posts,
+        'cname':cname
     }
     return render(request, 'posts_by_category.html',context)
     
